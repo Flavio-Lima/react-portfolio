78217a3959b7ec46d42c6002cb901acb665f89a8
Flavio Lima <flavio.lima981@gmail.com>
2023-05-30 09:48:01 -0300

    adiciona estrutura base

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { Hero } from './components/Hero'
+import { About } from './components/About'
+import { Services } from './components/Services'
+import { Projects } from './components/Projects'
+import { Skills } from './components/Skills'
+import { Testimonials } from './components/Testimonials'
+import { Contact } from './components/Contact'
+
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <main>
+      <Hero />
+      <About />
+      <Services />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   )
 }
 
