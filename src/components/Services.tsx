b582d726b77bdad5ffe14e7db01a8c3111d8b720
Flavio Lima <flavio.lima981@gmail.com>
2023-06-02 10:33:27 -0300

    adiciona section sobre mim desktop e mobile

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,7 +1,20 @@
 export function Services() {
   return (
-    <>
-      <h2 className="text-4xl underline font-bold">Services test</h2>
-    </>
+    <section className="container mx-auto my-12 max-w-4xl p-4">
+      <div className="p-4 text-center">
+        <p className="text-sm font-semibold uppercase text-blue-600">
+          O que faço de melhor
+        </p>
+
+        <h2 className="mb-2 font-bold text-blue-900">
+          <span className="mr-2 font-headline text-3xl">Meus</span>
+          <span className="font-handwriting text-4xl">Serviços</span>
+        </h2>
+
+        <p className="text-sm text-gray-600">
+          Posso atender uma gama completa de serviços, do front-end ao back-end com banco de dados e sistemas.
+        </p>
+      </div>
+    </section>
   )
 }
