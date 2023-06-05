bd4dde3704472d30cfabdd13a451523064540d03
Flavio Lima <flavio.lima981@gmail.com>
2023-06-05 10:09:24 -0300

    adiciona depoimentos desktop e mobile

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { About } from './components/About'
 import { Services } from './components/Services'
 import { Projects } from './components/Projects'
 import { Skills } from './components/Skills'
+import { Testimonials } from './components/Testimonials'
 import { Contact } from './components/Contact'
 
 import './App.css'
@@ -15,6 +16,7 @@ function App() {
       <Services />
       <Projects />
       <Skills />
+      <Testimonials />
       <Contact />
     </main>
   )
